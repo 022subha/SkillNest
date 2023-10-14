fc27ac572d204e23d5f56a0a887c04bf2c1f9887
Subhajit <022subha@gmail.com>
2023-10-14 11:57:31 +0530

    Routes Setup Done

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
